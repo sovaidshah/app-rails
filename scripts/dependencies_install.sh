736276171a760187f39b3c583021caa27e02ae33
sovaidshah <syedsovaids@gmail.com>
2022-07-07 19:33:33 +0500

    Updated AppSpec 2

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/home/ubuntu/.rvm/gems/ruby-2.5.0/bin:/home/ubuntu/.rvm/gems/ruby-2.5.0@global/bin:/home/ubuntu/.rvm/rubies/ruby-2.5.0/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.rvm/bin:/home/ubuntu/.rvm/bin
-sudo kill -9 $(cat /var/www/my-app/tmp/pids/server.pid)
-cd /var/www/my-app/
+sudo kill -9 $(cat /var/www/app-rails/tmp/pids/server.pid)
+cd /var/www/app-rails/
 #sudo apt-get install ruby2.3-dev libffi-dev -y
 gem install nokogiri — — use-system-libraries
 bundle config build.nokogiri — use-system-libraries
