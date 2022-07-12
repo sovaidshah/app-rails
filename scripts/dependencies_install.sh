442b0187a8e0d3efdab153a02025fb1414ea2753
sovaidshah <syedsovaids@gmail.com>
2022-07-13 02:12:50 +0500

    Rails Updated 2

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=/home/ubuntu/.rvm/gems/ruby-2.5.0/bin:/home/ubuntu/.rvm/gems/ruby-2.5.0@global/bin:/home/ubuntu/.rvm/rubies/ruby-2.5.0/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.rvm/bin:/home/ubuntu/.rvm/bin
-sudo kill -9 $(cat /var/www/app-rails/tmp/pids/server.pid)
+#sudo kill -9 $(cat /var/www/app-rails/tmp/pids/server.pid)
 cd /var/www/app-rails/
 #sudo apt-get install ruby2.3-dev libffi-dev -y
 gem install nokogiri — — use-system-libraries
