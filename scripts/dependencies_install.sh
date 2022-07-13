6e37a079680ce2e19ace9192dd07abedbd6be378
sovaidshah <syedsovaids@gmail.com>
2022-07-13 20:42:12 +0500

    Rails updated 19 rails server

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -4,7 +4,7 @@ sudo kill -9 $(cat /opt/app-rails/tmp/pids/server.pid)
 cd /opt/app-rails
 git pull origin master
 ##sudo apt-get install ruby2.3-dev libffi-dev -y
-#sudo gem install nokogiri — — use-system-libraries
-#bundle config build.nokogiri — use-system-libraries
-#sudo gem install bundler — user-install
+sudo gem install nokogiri — — use-system-libraries
+bundle config build.nokogiri — use-system-libraries
+sudo gem install bundler — user-install
 sudo bundle install
