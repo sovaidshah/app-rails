7f9fb1aca7a8e33919d8ecb44131983c6634d3c3
sovaidshah <syedsovaids@gmail.com>
2022-07-14 20:07:43 +0500

    Rails Updated

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 #export PATH=/home/ubuntu/.rvm/gems/ruby-2.5.0/bin:/home/ubuntu/.rvm/gems/ruby-2.5.0@global/bin:/home/ubuntu/.rvm/rubies/ruby-2.5.0/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.rvm/bin:/home/ubuntu/.rvm/bin
 #sudo kill -9 $(cat /opt/app-rails/tmp/pids/server.pid)
-cd /opt/codedeploy-agent
+cd /home/ubuntu/app-rails
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+~/.rbenv/bin/rbenv init
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+rbenv install 2.6.8
+rbenv global 2.6.8
 ##sudo apt-get install ruby2.3-dev libffi-dev -y
 #sudo gem install nokogiri — — use-system-libraries
 #bundle config build.nokogiri — use-system-libraries
-sudo gem install bundler — user-install
+#sudo gem install bundler — user-install
 sudo bundle install
+bundle exec passenger start -p 80 -a 0.0.0.0 -d
